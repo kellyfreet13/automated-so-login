daf0aa057a8930bc805c10bcad7cae60b68e88c2
Kelly Freet <kelly.freet13@yahoo.com>
2018-08-13 20:37:22 -0700

    first commit

---
+++ credentials.py
@@ -0,0 +1,5 @@
+email = ''
+pword = ''
+
+chrome_driver = 'C:/Users/Kelly/Desktop/webcrawler/chromedriver_win32/chromedriver'
+login_url = 'https://stackoverflow.com/users/login?ssrc=head&returnurl=https%3a%2f%2fstackoverflow.com%2f'
---
+++ login.py
@@ -0,0 +1,21 @@
+import time
+from selenium import webdriver
+import credentials as cred
+
+browser = webdriver.Chrome(cred.chrome_driver)
+browser.get(cred.login_url)
+time.sleep(4)  # give chrome some time to load
+
+# select html elements
+email_field = browser.find_element_by_id('email')
+pass_field = browser.find_element_by_id('password')
+submit_button = browser.find_element_by_id('submit-button')
+
+# input email/pass and login
+email_field.send_keys(cred.email)
+pass_field.send_keys(cred.pword)
+submit_button.click()
+
+# you know have your login for the day
+time.sleep(3)
+browser.quit()
